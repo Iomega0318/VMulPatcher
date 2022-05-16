8d85adf7ed6cdad7748770df6f172da026e80d9b
Iomega0318 <admin@honorshard.com>
2022-05-16 00:13:57 -0500

    Fixes Unknown identifier encountered (9409B1B712C31768)

--- VMulPatcher/Properties/AssemblyInfo.cs
+++ VMulPatcher/Properties/AssemblyInfo.cs
@@ -5,22 +5,22 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("VMulPatcher")]
-[assembly: AssemblyDescription("Venushja MUL/UOP Patcher")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Venushja studio")]
-[assembly: AssemblyProduct("VMulPatcher")]
-[assembly: AssemblyCopyright("Copyright © Venushja studio 2019")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+[assembly: AssemblyTitle("LegacyMUL-N")]
+[assembly: AssemblyDescription( "" )]
+[assembly: AssemblyConfiguration( "" )]
+[assembly: AssemblyCompany( "" )]
+[assembly: AssemblyProduct("LegacyMUL-N")]
+[assembly: AssemblyCopyright( "Copyright ©  2012" )]
+[assembly: AssemblyTrademark( "" )]
+[assembly: AssemblyCulture( "" )]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
+[assembly: ComVisible( false )]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("e82dcc24-b680-4a1d-895e-d4f01e782362")]
+[assembly: Guid( "34dc950a-9b7f-4e95-a03b-e27ff5053d62" )]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.2.0.0")]
-[assembly: AssemblyFileVersion("0.2.0.0")]
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
